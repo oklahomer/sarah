e0293510b6051582af9c308df7c07da5f5fb020a
Oklahomer <hagiwara.go@gmail.com>
2016-01-11 23:25:50 +0900

    Add sample plugin

--- sarah/bot/plugins/echo.py
+++ sarah/bot/plugins/echo.py
@@ -2,6 +2,7 @@
 from typing import Dict
 from sarah.bot.hipchat import HipChat
 from sarah.bot.slack import Slack
+from sarah.bot.gitter import Gitter
 from sarah.bot.values import CommandMessage
 
 
@@ -14,4 +15,10 @@ def hipchat_echo(msg: CommandMessage, config: Dict) -> str:
 # noinspection PyUnusedLocal
 @Slack.command('.echo')
 def slack_echo(msg: CommandMessage, config: Dict) -> str:
+    return msg.text
+
+
+# noinspection PyUnusedLocal
+@Gitter.command('.echo')
+def gitter_echo(msg: CommandMessage, config: Dict) -> str:
     return msg.text
